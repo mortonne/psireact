80fa8b4ecd68651bc472d3c33381cb20239bd9bc
Neal Morton <mortonne@gmail.com>
2020-02-16 20:11:40 -0600

    basic setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="psireact",
+    version="0.0.1",
+    author="Neal Morton",
+    author_email="mortonne@gmail.com",
+    description="Response time modeling of psychology experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mortonne/psireact",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'seaborn',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ]
+)
