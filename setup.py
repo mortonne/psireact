99ad16a0a1114ee6c6382ca21f29dc024c79712b
Neal Morton <mortonne@gmail.com>
2020-11-30 10:49:58 -0600

    build: bump patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="psireact",
-    version="0.2.11",
+    version="0.2.12",
     author="Neal Morton",
     author_email="mortonne@gmail.com",
     description="Response time modeling of psychology experiments",
